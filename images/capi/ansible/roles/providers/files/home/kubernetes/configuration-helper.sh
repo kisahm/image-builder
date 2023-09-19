805466e60d9f0a1d8678271bccfd605ecec8d573
Kim-Norman Sahm <kimnorman.sahm@gmail.com>
2023-09-19 12:53:51 +0200

    fix kubelet

--- images/capi/ansible/roles/providers/files/home/kubernetes/configuration-helper.sh
+++ images/capi/ansible/roles/providers/files/home/kubernetes/configuration-helper.sh
@@ -1635,9 +1635,8 @@ function start-kubelet {
   local kubelet_opts="${KUBELET_ARGS} ${KUBELET_CONFIG_FILE_ARG:-} ${kubelet_cgroup_driver:-}"
   echo "KUBELET_OPTS=\"${kubelet_opts}\"" > "${kubelet_env_file}"
   echo "KUBE_COVERAGE_FILE=\"/var/log/kubelet.cov\"" >> "${kubelet_env_file}"
-  
+
   systemctl daemon-reload
-  systemctl start kubelet.service
 }
 
 # This function assembles the node problem detector systemd service file and
